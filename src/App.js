2b3b0235bccf09a1ec2305296ae50415aabc2da3
hermansteen <32453258+hermansteen@users.noreply.github.com>
2020-09-16 15:18:36 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import countries from 'world-countries'
 
+countries.sort((a, b) => b.area - a.area)
+
+const CountryInfo = props => {
+  const areaScale = countries[0].area / 100;
+  const calculatedWidth = (props.data.area / areaScale) + '%'
+
+
+  return (
+    <div className="CountryInfo">
+      <div className="country">
+        {props.data.name.common}  {props.data.area}km<sup>2</sup></div>
+      <div className="bar" style={{width:calculatedWidth}}></div>
+    </div>
+  )
+}
+
+console.log(countries);
 function App() {
   return (
+    countries.map((country)=>
     <div className="App">
-      
+      <CountryInfo data={country}/>
     </div>
+    )
   );
 }
 
